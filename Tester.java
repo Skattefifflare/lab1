56d0cb351647a8a133932ac019a210f2495b544e
srisahitichenna <srisahitichenna@gmail.com>
2026-01-29 15:58:27 +0100

    _

--- Tester.java
+++ Tester.java
@@ -1,5 +1,5 @@
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -7,52 +7,34 @@ import org.junit.jupiter.api.Test;
 public class Tester {
 
     @Test
-    public void testMove() {
+    public void testMove(){
         var car = new Saab95();
         car.startEngine();
         car.move();
-        assertEquals(0.1, car.getY());
-
-    }
-
-    public static void main(String[] args) {
-
+        assertEquals(0.1, car.getX());
     }
 
     @Test
-    public void testTurnLeft() {
-        var car = new Saab95();
-        car.startEngine();
-        car.move();
-        car.turnLeft();
-        assertEquals(3, car.getDirection());
-        car.turnLeft();
-        assertEquals(2, car.getDirection());
-        car.turnLeft();
-        assertEquals(1, car.getDirection());
-        car.turnLeft();
-        assertEquals(0, car.getDirection());
-
-    }
-
-    @Test
-    public void testTurnRight() {
-        var car = new Saab95();
-        car.startEngine();
-        car.move();
-        car.turnRight();
-        assertEquals(1, car.getDirection());
-        car.turnRight();
-        assertEquals(2, car.getDirection());
-        car.turnRight();
-        assertEquals(3, car.getDirection());
-        car.turnRight();
-        assertEquals(0, car.getDirection());
-
-
-
+    public void testGasAndBrake(){
+        var car2 = new Volvo240();
+        var car3 = new Volvo240();
+        car2.startEngine();
+        car2.move();
+        var old_speed = car2.getCurrentSpeed();
+        car2.gas(0.9);
+        assertTrue(old_speed < car2.getCurrentSpeed());//Gas
+        var incSpeed = car2.getCurrentSpeed();
+        car2.brake(0.4);
+        assertTrue(incSpeed > car2.getCurrentSpeed());//Brake
     }
 
 
+
+
+    public static void main (String[] args){
+
+        Tester t = new Tester();
+        t.testMove();
+    }
+    
 }
-
