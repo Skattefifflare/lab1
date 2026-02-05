d09fd8cab05cb8dd60bb515859df8a2b4d2f9aed
Elias Fazelia <fazelia@chalmers.se>
2026-02-05 11:10:37 +0100

    _

---
+++ Transportbil.java
@@ -0,0 +1,37 @@
+import java.awt.*;
+
+public class Transportbil extends Car implements Truck  {
+
+
+    public Transportbil(int nrDoors, double enginePower, Color color, String modelName){
+        super(nrDoors, enginePower, color, modelName);
+    }
+
+    private boolean flakAngle;
+    public boolean getFlakAngle() {
+        return flakAngle;
+    }
+
+    @Override
+    public double speedFactor(){
+        if (flakAngle) return 0;
+        else return 1;
+    }
+
+
+    public void DecrementFlak() {
+        if(getCurrentSpeed() == 0){
+            if (flakAngle){
+                flakAngle = false;
+            }
+        }
+    }
+
+    public void IncrementFlak() {
+        if(getCurrentSpeed() == 0){
+            if(!flakAngle) {
+                flakAngle = true;
+            }
+        }
+    }
+}
