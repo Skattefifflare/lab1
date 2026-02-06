bc34c4d66fc2b47ed1e91aff2794c2213387627f
Zunken <124305730+Skattefifflare@users.noreply.github.com>
2026-02-06 18:34:58 +0100

    Update Workshop.java

--- Workshop.java
+++ Workshop.java
@@ -4,7 +4,13 @@ import towing.Cargo;
 public class Workshop<E extends Car> {
     private Cargo<E> cars;
 
-    public Workshop(){
-        cars = new Cargo<E>(10);
+    public Workshop(int max_cars){
+        cars = new Cargo<E>(max_cars);
+    }
+    public void LeaveCar(E car){
+        cars.Add(car);
+    }
+    public E GetCar(){
+        return cars.Remove();
     }
 }
